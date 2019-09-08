42a144b42a78c9f7d3b588b2e97a546c2e2a4c4b
Kathy Maloney <ksm.maloney@gmail.com>
2019-09-08 10:22:45 -0600

    Add code for JavaScript Lesson 2-Declare JavaScript Variables, plus a comment on what I learned

---
+++ 5-Introduction-to-JavaScript/2-Declare-JavaScript-Variables.js
@@ -0,0 +1,25 @@
+/* I learned there are 7 data types in JavaScript:
+-object
+-boolean
+-undefined
+-null
+-number
+-string
+-symbol
+(together they make the acronym OBUNNSS)
+
+Strings are collections of characters. There may be numbers included in a string, such as "123 cats" or the string "12," but computers can't perform math on strings, only on numbers.
+
+Variables are names that represent data. They can contain any of the 7 data types. Also, they can store different values at different times.
+
+Rules for variable names:
+- they can be numbers, letters, $ or_, but cannot contain spaces or start with a number
+- write variables in camelCase
+*/
+
+
+// Example
+var ourName;
+
+// Declare myName below this line
+var myName;
