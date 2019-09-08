d22e79cb757106c17c3753e3b0ade8c7de69c23e
Kathy Maloney <ksm.maloney@gmail.com>
2019-09-08 17:20:51 -0600

    Add code for JavaScript Lesson 8-Subtract One Number from Another w/ JavaScript, and add a note about what I learned

---
+++ 5-Introduction-to-JavaScript/8-Subtract-One-Number-from-Another-with-JavaScript.js
@@ -0,0 +1,4 @@
+// For subtraction, JavaScript uses the usual " - " sumbol. Also, I noticed that the comment in these exercises has been placed right after the operation.
+
+
+var difference = 45 - 33;
