cd310f5a175ddd594eaa5769cd81e46a52ca0365
Kathy Maloney <ksm.maloney@gmail.com>
2019-09-08 17:54:22 -0600

    Add code for JavaScript Lesson 11- Increment a Number w/ JavaScript, with a note about what I learned

---
+++ 5-Introduction-to-JavaScript/11-Increment-a-Number-with-JavaScript.js
@@ -0,0 +1,10 @@
+/* To increment, or add one to a variable, use the "++" operator. This method eliminates use of the " = ". The entire line becomes: "variable name", with "++ " next to it, no spaces in between.
+
+( "i = i +1;" becomes " i++; ")
+
+*/
+
+var myVar = 87;
+
+// Only change code below this line
+myVar++;
