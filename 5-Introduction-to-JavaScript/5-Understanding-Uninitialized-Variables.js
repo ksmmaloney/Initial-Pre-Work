9c770fb85ba4e4d0b5638d31cf97caf704fefbab
Kathy Maloney <ksm.maloney@gmail.com>
2019-09-08 13:42:59 -0600

    Add code for JavaScript Lesson 5-Understanding Uninitialized Variables, with a note about what I learned

---
+++ 5-Introduction-to-JavaScript/5-Understanding-Uninitialized-Variables.js
@@ -0,0 +1,17 @@
+/* Because a variable is first declared without a value, ( or "undefined") you have to give it a value if you want to do mathematical operations on it. Otherwise you'll get NaN, "Not a Number."
+
+If you concatenate a string with an undefined variable, you'll get a string of "undefined".
+
+I learned that the data types that got stored in a and b are numbers, and the data type of c is a string.
+*/
+
+// Initialize these three variables
+var a = 5;
+var b = 10;
+var c = "I am a";
+
+// Do not change code below this line
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
