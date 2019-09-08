7ad1f08124d23ed5aacb1c50918b86ca4e488e61
Kathy Maloney <ksm.maloney@gmail.com>
2019-09-08 13:16:59 -0600

    Add code for JavaScript Lesson 4-Initializing Variables w/the Assignment Operator, plus a note on what I learned

---
+++ 5-Introduction-to-JavaScript/4-Initializing-Variables-with-the-Assignment-Operator.js
@@ -0,0 +1,9 @@
+// You can set a value to a variable at the time that you declare the variable. Just add " = " followed by the value and end with ;
+
+
+// Example
+var ourVar = 19;
+
+// Only change code below this line
+
+var a = 9;
