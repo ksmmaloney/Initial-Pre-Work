8fadb86e324a2b67587345e5b2274b9ce4d251c7
Kathy Maloney <ksm.maloney@gmail.com>
2019-09-08 21:29:36 -0600

    Add code for JavaScript Lesson 19-Compound Assignment w/Augmented Multiplication, and add a note about what I learned

---
+++ 5-Introduction-to-JavaScript/19-Compound-Assignment-With-Augmented-Multiplication.js
@@ -0,0 +1,12 @@
+//  The *= operator will perform a similar task as += and -=, but with multiplication this time.
+
+
+var a = 5;
+var b = 12;
+var c = 4.6;
+
+// Only modify code below this line
+
+a *= 5;
+b *= 3;
+c *= 10;
