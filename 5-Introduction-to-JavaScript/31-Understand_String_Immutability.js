75697322b5d0bc7e4713484e24a72e5f598972c3
Kathy Maloney <ksm.maloney@gmail.com>
2019-09-09 17:26:15 -0600

    Add code for JavaScript Lesson 31-Understand String Immutability, plus a note about what I learned

---
+++ 5-Introduction-to-JavaScript/31-Understand_String_Immutability.js
@@ -0,0 +1,8 @@
+// String immutability means that the characters in a string cannot be changed. The value of a variable can always change, but if that value is a string, the only way to change it is to assign a new string.
+
+// Setup
+var myStr = "Jello World";
+
+// Only change code below this line
+
+myStr = "Hello World"; // Fix Me
