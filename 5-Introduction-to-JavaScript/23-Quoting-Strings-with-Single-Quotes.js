d5cd4c14a1143177603b60bed7a9e50b10cae2e1
Kathy Maloney <ksm.maloney@gmail.com>
2019-09-08 22:35:28 -0600

    Add code for JavaScript Lesson 23-Quoting Strings w/ Single Quotes, plus a note about what I learned

---
+++ 5-Introduction-to-JavaScript/23-Quoting-Strings-with-Single-Quotes.js
@@ -0,0 +1,6 @@
+/*  JavaScript is flexible in the way that it lets you use quote marks in string values. You can use either single or double quotes as long as you start and end with the same type.
+
+If you are inserting text that uses some kind of quote marks-- like as in a contraction (won't or can't), or a website address in quotes, or a quote which is something someone is saying something, "I have a dream"--you can avoid problems if you use one type of quotes at the beginning and end of a string, and a different kind of quotes for your content.
+*/
+
+var myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
