2f466a77f322b4ee6eb72fe7c1c1c825db6a8c20
Kathy Maloney <ksm.maloney@gmail.com>
2019-09-08 21:04:21 -0600

    Add code for JavaScript Lesson 17-Compound Assignment w/ Augmented Addition, plus a note on what I learned

---
+++ 5-Introduction-to-JavaScript/17-Compound-Assignment-With-Augmented-Addition.js
@@ -0,0 +1,12 @@
+//  JavaScript has operators that combine a mathematical operation and a value assignment in one step, like a shortcut. For addition, the operator is " += ". This operator will add a number to a variable and will assign the value in one step. 
+
+
+var a = 3;
+var b = 17;
+var c = 12;
+
+// Only modify code below this line
+
+a += 12;
+b += 9;
+c += 7;
