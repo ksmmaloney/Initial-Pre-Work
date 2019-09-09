553684c0f046800d50948e0027d34f1772bda13e
Kathy Maloney <ksm.maloney@gmail.com>
2019-09-09 15:10:35 -0600

    Add code for JavaScript Lesson 29-Find the Length of a String, plus a comment on what I learned

---
+++ 5-Introduction-to-JavaScript/29-Find_the_Length_of_a_String.js
@@ -0,0 +1,15 @@
+//  If you need to find the length of a string value in terms of how many characters, you write .length after the string variable. In this example, what actually gets stored in lastNameLength is the value 8, and not the name Lovelace.
+
+// Example
+var firstNameLength = 0;
+var firstName = "Ada";
+
+firstNameLength = firstName.length;
+
+// Setup
+var lastNameLength = 0;
+var lastName = "Lovelace";
+
+// Only change code below this line.
+
+lastNameLength = lastName.length;
