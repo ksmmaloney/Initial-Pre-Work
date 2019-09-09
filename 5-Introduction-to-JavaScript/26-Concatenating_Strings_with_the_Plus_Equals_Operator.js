72863c91e4bdb08ec8c25f5bf82ea0f65b68bbfd
Kathy Maloney <ksm.maloney@gmail.com>
2019-09-08 23:30:50 -0600

    Add code for JavaScript Lesson 26-Concatenating Strings w/the Plus Equals Operator, and add a note about what I learned

---
+++ 5-Introduction-to-JavaScript/26-Concatenating_Strings_with_the_Plus_Equals_Operator.js
@@ -0,0 +1,13 @@
+/* Similar to working with numbers, you can use the " += " operator to concatenate strings. You could use this to break up a long string onto several lines and avoid having one really long line of code.
+
+I learned here to make sure and put an extra space after the period in the first sentence (between the period and the quote mark).
+*/
+
+// Example
+var ourStr = "I come first. ";
+ourStr += "I come second.";
+
+// Only change code below this line
+
+var myStr = "This is the first sentence. ";
+myStr += "This is the second sentence.";
