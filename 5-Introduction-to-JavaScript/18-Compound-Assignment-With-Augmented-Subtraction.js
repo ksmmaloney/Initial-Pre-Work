b4dcc59484cb24e7741fe8e4b15a717f2ce910a2
Kathy Maloney <ksm.maloney@gmail.com>
2019-09-08 21:21:44 -0600

    Add code for JavaScript Lesson 18-Compound Assignment w/ Augmented Subtraction, and add a note about what I learned

---
+++ 5-Introduction-to-JavaScript/18-Compound-Assignment-With-Augmented-Subtraction.js
@@ -0,0 +1,11 @@
+//  Similar to the " += " operator, you can use " -= " to subtract a number from a variable and assign it at the same time.
+
+var a = 11;
+var b = 9;
+var c = 3;
+
+// Only modify code below this line
+
+a -= 6;
+b -= 15;
+c -= 1;
