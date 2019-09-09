7ac56e5097976fd61e3d9c02b70fc5a62d7657b3
Kathy Maloney <ksm.maloney@gmail.com>
2019-09-08 21:47:11 -0600

    Add code for JavaScript Lesson 21-Declare String Variables, plus a note about what I learned

---
+++ 5-Introduction-to-JavaScript/21-Declare-String-Variables.js
@@ -0,0 +1,10 @@
+// I learned in this challenge about declaring another kind of variable: the data type "string". A string is a series of zero or more characters enclosed in single or double quotes.
+
+
+// Example
+var firstName = "Alan";
+var lastName = "Turing";
+
+// Only change code below this line
+var myFirstName = "Kathy";
+var myLastName = "Maloney";
