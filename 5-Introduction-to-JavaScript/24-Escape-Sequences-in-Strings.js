e91f16cebc79581eb3cc36805558827ea6d5c315
Kathy Maloney <ksm.maloney@gmail.com>
2019-09-08 23:00:02 -0600

    Add code for JavaScript Lesson 24-Escape Sequences in Strings, and a note about what I learned

---
+++ 5-Introduction-to-JavaScript/24-Escape-Sequences-in-Strings.js
@@ -0,0 +1,3 @@
+//  In addition to "escaping" quote marks, you can escape things like backslashes and "invisible" formatting functions that go along with inserting text--things like adding a new line, using a tab, backspace, etc. Escaping allows you to insert these type of keyboard strokes without having errors in your code. All of these require putting a backslash \in front of the special characters.
+
+var myStr = "FirstLine\n\t\\SecondLine\nThirdLine"; // Change this line
