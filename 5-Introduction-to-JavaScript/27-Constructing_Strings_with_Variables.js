0713f43d5ae26938aeb9b49f639c2a40f86ce2a7
Kathy Maloney <ksm.maloney@gmail.com>
2019-09-08 23:41:35 -0600

    Add code for JavaScript Lesson 27-Constructing Strings w/ Variables, plus a note about what I learned

---
+++ 5-Introduction-to-JavaScript/27-Constructing_Strings_with_Variables.js
@@ -0,0 +1,9 @@
+// JavaScript allows you to insert variables inside a string. You need to use the concatenation operator ( + ) at each end of the inserted variable, with appropriate spacing. 
+
+// Example
+var ourName = "freeCodeCamp";
+var ourStr = "Hello, our name is " + ourName + ", how are you?";
+
+// Only change code below this line
+var myName = "Kathy";
+var myStr = "My name is " + myName + " and I am well!";
