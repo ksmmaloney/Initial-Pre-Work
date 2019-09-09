4c1079e1112bd28331198b73c496e4c4fb926658
Kathy Maloney <ksm.maloney@gmail.com>
2019-09-08 20:40:54 -0600

    Add code for JavaScript Lesson 16-Finding a Remainder in JavaScript, and add a note about what I learned

---
+++ 5-Introduction-to-JavaScript/16-Finding-a-Remainder-in-JavaScript.js
@@ -0,0 +1,5 @@
+// When you divide two numbers there is often a remainder. The remainder operator produces this remainder and sets it as a value. The symbol is " % ".
+
+// Only change code below this line
+
+var remainder = 11 % 3;
