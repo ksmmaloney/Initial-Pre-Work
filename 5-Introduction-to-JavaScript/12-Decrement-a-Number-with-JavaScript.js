23489ca8d746ff424fc81ee6a238e27f122f835c
Kathy Maloney <ksm.maloney@gmail.com>
2019-09-08 18:34:35 -0600

    Add code for JavaScript Lesson 12-Decrement a Number w/ JavaScript, plus a comment on what I learned

---
+++ 5-Introduction-to-JavaScript/12-Decrement-a-Number-with-JavaScript.js
@@ -0,0 +1,7 @@
+// Similar to " " ++ which increments a variable by one, you can use " -- " to decrement a variable by one. This method also eliminates use of the " = ". The entire line becomes: "variable name", with " -- " next to it, no spaces in between.
+
+
+var myVar = 11;
+
+// Only change code below this line
+myVar--;
