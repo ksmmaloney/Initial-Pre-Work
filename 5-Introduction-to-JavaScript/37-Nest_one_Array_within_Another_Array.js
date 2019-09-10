dc5eefa6e73e577e0859d46973b8bb790df06e1d
Kathy Maloney <ksm.maloney@gmail.com>
2019-09-09 20:46:08 -0600

    Add code for JavaScript Lesson 37-Nest One Array within Another Array, plus a note on what I learned

---
+++ 5-Introduction-to-JavaScript/37-Nest_one_Array_within_Another_Array.js
@@ -0,0 +1,7 @@
+// Arrays can be nested inside other arrays. Just create the inner arrays with brackets, and put a comma between each inner array. Also called a multidimensional array.
+
+// Example
+var ourArray = [["the universe", 42], ["everything", 101010]];
+
+// Only change code below this line.
+var myArray = [["Aspen", 81611], ["Snowmass", 81615]];
