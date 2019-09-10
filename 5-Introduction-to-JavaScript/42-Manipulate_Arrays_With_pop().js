d67f5652a7d3d0b5abe8568ab597784c9e6129bc
Kathy Maloney <ksm.maloney@gmail.com>
2019-09-09 23:51:17 -0600

    Add code for JavaScript Lesson 42-Manipulate Arrays with  pop(), and add a comment about what I learned

---
+++ 5-Introduction-to-JavaScript/42-Manipulate_Arrays_With_pop().js
@@ -0,0 +1,13 @@
+// You can change the data at the very end of an array with .pop(). When you use .pop(), the data pops off the end data and it lands inside a new variable that you've created. The element that gets popped off can be an array or a number or a string, any type of entry.
+
+
+// Example
+var ourArray = [1,2,3];
+var removedFromOurArray = ourArray.pop();
+// removedFromOurArray now equals 3, and ourArray now equals [1,2]
+
+// Setup
+var myArray = [["John", 23], ["cat", 2]];
+
+// Only change code below this line.
+var removedFromMyArray = myArray.pop();
