f989221ec24d2b24a2c13ee63fb8584b29291eb9
Kathy Maloney <ksm.maloney@gmail.com>
2019-09-10 11:19:20 -0600

    Add code for JavaScript Lesson 46-Write Reusable JavaScript w/Functions, plus a note about what I learned

---
+++ 5-Introduction-to-JavaScript/46-Write_Reusable_JavaScript_with_Functions.js
@@ -0,0 +1,23 @@
+/* Functions are a feature that allow you to use code over and over.
+1. Create a function using properCamelCase and closed parentheses at the end:  function functionName ()
+2. Write code inside curly brackets { code, code, code;}
+3. Call the function using its name followed by closed parentheses and a semicolon ;
+
+functionName();
+
+Whenever you call the function, the code inside the curly braces gets executed.
+*/
+
+// Example
+function ourReusableFunction() {
+  console.log("Heyya, World");
+}
+
+ourReusableFunction();
+
+// Only change code below this line
+function reusableFunction() {
+    console.log("Hi World");
+}
+
+reusableFunction();
