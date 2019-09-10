149e6e8fdee9ea71fd58105aff1d46f4e74c7507
Kathy Maloney <ksm.maloney@gmail.com>
2019-09-09 20:37:07 -0600

    Add code for JavaScript Lesson 36-Store Multiple Values in one Variable using Arrays, plus a note on what I learned

---
+++ 5-Introduction-to-JavaScript/36-Store_Multiple_Values_in_one_Variable_using_JavaScript_Arrays.js
@@ -0,0 +1,8 @@
+/*  An array (a row of things) allows you to store more than one value at a time. You create an array using brackets. Each value goes inside quotes, and each is separated by commas.
+*/
+
+// Example
+var ourArray = ["John", 23];
+
+// Only change code below this line.
+var myArray = ["Hockey jersey", 646];
