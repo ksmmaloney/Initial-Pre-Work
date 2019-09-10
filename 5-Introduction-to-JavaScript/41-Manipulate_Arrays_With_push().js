069f49d45e84415e1c78ec609f11df579bcc28b9
Kathy Maloney <ksm.maloney@gmail.com>
2019-09-09 23:38:08 -0600

    Add code for JavaScript Lesson 41-Manipulate Arrays with push(), with a note about what I learned

---
+++ 5-Introduction-to-JavaScript/41-Manipulate_Arrays_With_push().js
@@ -0,0 +1,12 @@
+//  You can add data to the end of an array using " .push() "  You just add .push( ) directly after a variable name, and put a value inside the parentheses.  The function pushes one or more parameters onto the end of an array (but still inside the array end bracket).
+
+// Example
+var ourArray = ["Stimpson", "J", "cat"];
+ourArray.push(["happy", "joy"]);
+// ourArray now equals ["Stimpson", "J", "cat", ["happy", "joy"]]
+
+// Setup
+var myArray = [["John", 23], ["cat", 2]];
+
+// Only change code below this line.
+myArray.push(["dog", 3]);
