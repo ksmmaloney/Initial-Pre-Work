c71fa2a71f416e64ce47b17a1c802794b6e6c5d4
Kathy Maloney <ksm.maloney@gmail.com>
2019-09-09 22:40:11 -0600

    Add code for JavaScript Lesson 38-Access Array Data w/Indexes, plus a note on what I learned

---
+++ 5-Introduction-to-JavaScript/38-Access_Array_Data_with_Indexes.js
@@ -0,0 +1,11 @@
+// Just as there's a method to access a character in a string, there's also a way to access data inside an array. "Array indexes" allow you to locate an ENTRY in an array, while a string index lets you find a CHARACTER. Array indexes also use bracket notation, and like strings, the first element is numbered 0.
+
+// Example
+var ourArray = [50,60,70];
+var ourData = ourArray[0]; // equals 50
+
+// Setup
+var myArray = [50,60,70];
+
+// Only change code below this line.
+var myData = myArray[0];
