08c75df09c9f27eccab23e96b930380fd10cf775
Kathy Maloney <ksm.maloney@gmail.com>
2019-09-09 20:14:31 -0600

    Add code for JavaScript Lesson 35-Word Banks, plus a note about what I learned

---
+++ 5-Introduction-to-JavaScript/35-Word_Blanks.js
@@ -0,0 +1,14 @@
+// I learned that spaces matter! 
+
+
+
+function wordBlanks(myNoun, myAdjective, myVerb, myAdverb) {
+  // Your code below this line
+  var result = "My " + myNoun + " Red is so " + myAdjective + " that he " + myVerb + " too " + myAdverb + " and crashed into the fence.";
+
+  // Your code above this line
+  return result;
+}
+
+// Change the words here to test your function
+wordBlanks("dog", "big", "ran", "quickly");
