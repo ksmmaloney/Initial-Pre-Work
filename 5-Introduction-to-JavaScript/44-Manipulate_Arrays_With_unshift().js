33f3880091092b6444097a976e6c734aaf2d06eb
Kathy Maloney <ksm.maloney@gmail.com>
2019-09-10 00:16:38 -0600

    Add code for JavaScript Lesson 44-Manipulate Arrays with unshift, plus a note about what I learned

---
+++ 5-Introduction-to-JavaScript/44-Manipulate_Arrays_With_unshift().js
@@ -0,0 +1,15 @@
+// You can use " unshift() " to add elements at the beginning of an array. It's similar to push, but it places elements at the beginning instead of the end.
+
+
+//Example
+var ourArray = ["Stimpson", "J", "cat"];
+ourArray.shift(); // ourArray now equals ["J", "cat"]
+ourArray.unshift("Happy");
+// ourArray now equals ["Happy", "J", "cat"]
+
+// Setup
+var myArray = [["John", 23], ["dog", 3]];
+myArray.shift();
+
+// Only change code below this line.
+myArray.unshift(["Paul", 35]);
