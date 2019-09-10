47c7d194b34551b7d97502e4d90c1520d673b803
Kathy Maloney <ksm.maloney@gmail.com>
2019-09-10 12:09:38 -0600

    Add code for JavaScript Lesson 48-Global Scope and Functions, with a note about what I learned

---
+++ 5-Introduction-to-JavaScript/48-Global_Scope_and_Functions.js
@@ -0,0 +1,22 @@
+// The term "scope" refers to the visibility of a variable. When variables are defined outside of a function block, they are said to have "Global scope"--they are visible everywhere in the JavaScript code. To avoid unintended consequences in your code, variables should always be defined using "var".
+
+
+// Declare your variable here
+var myGlobal = 10;
+
+function fun1() {
+  // Assign 5 to oopsGlobal Here
+  oopsGlobal = 5;
+}
+
+// Only change code above this line
+function fun2() {
+  var output = "";
+  if (typeof myGlobal != "undefined") {
+    output += "myGlobal: " + myGlobal;
+  }
+  if (typeof oopsGlobal != "undefined") {
+    output += " oopsGlobal: " + oopsGlobal;
+  }
+  console.log(output);
+}
