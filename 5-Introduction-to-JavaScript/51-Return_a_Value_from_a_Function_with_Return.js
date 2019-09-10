fbd15ba843ed9d8eeae74d7d60503eef1a7537ce
Kathy Maloney <ksm.maloney@gmail.com>
2019-09-10 12:57:11 -0600

    Add code for JavaScript Lesson 51-Return a Value from a Function w/Return, plus a note about what I learned

---
+++ 5-Introduction-to-JavaScript/51-Return_a_Value_from_a_Function_with_Return.js
@@ -0,0 +1,14 @@
+//  You can write a "return" statement within a function to output data.
+
+
+// Example
+function minusSeven(num) {
+  return num - 7;
+}
+
+// Only change code below this line
+function timesFive(num) {
+  return num * 5;
+}
+
+console.log(timesFive(5));
