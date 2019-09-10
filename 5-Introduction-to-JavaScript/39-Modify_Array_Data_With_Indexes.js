944399244ba5c49e636241148eb0638e5994b473
Kathy Maloney <ksm.maloney@gmail.com>
2019-09-09 22:54:04 -0600

    Add code for JavaScript Lesson 39-Modify Array Data w/Indexes, and add a comment on what I learned

---
+++ 5-Introduction-to-JavaScript/39-Modify_Array_Data_With_Indexes.js
@@ -0,0 +1,12 @@
+//  Array entries can be changed and are said to be "mutable," unlike strings which are immutable. Add one set of square brackets on the right side of the array name, with no spaces in between. Don't forget that counting starts at 0.
+
+
+// Example
+var ourArray = [18,64,99];
+ourArray[1] = 45; // ourArray now equals [18,45,99].
+
+// Setup
+var myArray = [18,64,99];
+
+// Only change code below this line.
+myArray[0] = 45;
