dfca3193907dcb4669dc2ca274d7a216b7fd5cb0
Kathy Maloney <ksm.maloney@gmail.com>
2019-09-10 16:02:31 -0600

    Add code for JavaScript Lesson 55-Understanding Boolean Values, with a note about what I learned

---
+++ 5-Introduction-to-JavaScript/55-Understanding_Boolean_Values.js
@@ -0,0 +1,10 @@
+//  Boolean is another data type. The value of a Boolean can only be true or false. Values are never written in quotes.
+
+function welcomeToBooleans() {
+
+// Only change code below this line.
+
+return true; // Change this line
+
+// Only change code above this line.
+}
