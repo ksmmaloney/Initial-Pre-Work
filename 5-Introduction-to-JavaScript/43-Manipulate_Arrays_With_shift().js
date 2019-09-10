8b599d91d123250a14146863e94761c710d82b41
Kathy Maloney <ksm.maloney@gmail.com>
2019-09-09 23:58:30 -0600

    Add code for JavaScript Lesson 43-Manipulate Arrays with  shift, plus a note about what I learned

---
+++ 5-Introduction-to-JavaScript/43-Manipulate_Arrays_With_shift().js
@@ -0,0 +1,12 @@
+// You can use .shift() to remove the first element of an array instead of the last (like when using .pop()  ). Same method as .pop().
+
+// Example
+var ourArray = ["Stimpson", "J", ["cat"]];
+var removedFromOurArray = ourArray.shift();
+// removedFromOurArray now equals "Stimpson" and ourArray now equals ["J", ["cat"]].
+
+// Setup
+var myArray = [["John", 23], ["dog", 3]];
+
+// Only change code below this line.
+var removedFromMyArray = myArray.shift();
