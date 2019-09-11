e8df65bb3e891b30ee42a3db24ba587ffeed2b51
Kathy Maloney <ksm.maloney@gmail.com>
2019-09-10 22:26:47 -0600

    Add code for JavaScript Lesson 70-Logical Order in If Else Statements, with a note about what I learned

---
+++ 5-Introduction-to-JavaScript/70-Logical_Order_in_If_Else_Statements.js
@@ -0,0 +1,14 @@
+// You can link together "if" statements with "else if" statements if you have more than two conditions.
+
+
+function testElseIf(val) {
+  if (val > 10) {
+    return "Greater than 10";
+  } else if (val < 5) {
+    return "Smaller than 5";
+  } else {
+    return "Between 5 and 10";
+  }
+}
+// Change this value to test
+console.log(testElseIf(10));
