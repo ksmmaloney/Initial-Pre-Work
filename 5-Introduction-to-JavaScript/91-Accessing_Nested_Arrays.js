ac5e7add249995ab24bc9ff50af74b1b1530da52
Kathy Maloney <ksm.maloney@gmail.com>
2019-09-12 16:43:02 -0600

    Add code for JavaScript Lesson 91-Accessing Nested Arrays, plus a comment on what I learned

---
+++ 5-Introduction-to-JavaScript/91-Accessing_Nested_Arrays.js
@@ -0,0 +1,25 @@
+//  You can link together, or chain, array bracket notation in order to access nested arrays. 
+
+// Setup
+var myPlants = [
+  {
+    type: "flowers",
+    list: [
+      "rose",
+      "tulip",
+      "dandelion"
+    ]
+  },
+  {
+    type: "trees",
+    list: [
+      "fir",
+      "pine",
+      "birch"
+    ]
+  }
+];
+
+// Only change code below this line
+
+var secondTree = myPlants[1].list[1]; // Change this line
