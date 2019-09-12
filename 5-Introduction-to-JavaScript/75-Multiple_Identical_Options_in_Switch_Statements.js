f60667e69af7d25a841b82ec4cf56b5631196d32
Kathy Maloney <ksm.maloney@gmail.com>
2019-09-11 19:22:39 -0600

    Add code for JavaScript Lesson 75-Multiple Identical Options in Switch Statements, plus a note on what I learned

--- 5-Introduction-to-JavaScript/75-Multiple_Identical_Options_in_Switch_Statements.js
+++ 5-Introduction-to-JavaScript/75-Multiple_Identical_Options_in_Switch_Statements.js
@@ -1,1 +1,30 @@
+// This option is for when there's more than one input that has the same output, like when there's a range of values. Also, note that after you declare a case, you put a colon after it, not a semi colon.
 
+function sequentialSizes(val) {
+  var answer = "";
+  // Only change code below this line
+  switch(val)  {
+    case 1:
+    case 2:
+    case 3:
+      return "Low";
+      break;
+    case 4:
+    case 5:
+    case 6:
+      return "Mid";
+      break;
+    case 7:
+    case 8:
+    case 9:
+      return "High";
+      break;
+
+  }
+
+  // Only change code above this line
+  return answer;
+}
+
+// Change this value to test
+sequentialSizes(1);
