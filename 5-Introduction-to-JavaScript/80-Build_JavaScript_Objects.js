0a5bb9288236b5f4f5ecaf381f9c4e19e14eb9f8
Kathy Maloney <ksm.maloney@gmail.com>
2019-09-12 10:42:12 -0600

    Add code for JavaScript Lesson 80-Build JavaScript Objects, plus a comment on what I learned

---
+++ 5-Introduction-to-JavaScript/80-Build_JavaScript_Objects.js
@@ -0,0 +1,20 @@
+// I learned that objects are meant to represent objects like in real life. Objects have properties that can describe them, and these properties are what you use to access the data in an object. Strings and numbers can both be stored as properties.
+
+
+// Example
+var ourDog = {
+  "name": "Camper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"]
+};
+
+// Only change code below this line.
+
+var myDog = {
+  "name": "Red",
+  "legs": 4,
+  "tails": .5,
+  "friends": ["female people", "male people under age 10", "other dogs"]
+
+};
