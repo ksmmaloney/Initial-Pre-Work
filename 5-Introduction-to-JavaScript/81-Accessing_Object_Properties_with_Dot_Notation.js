541a9396104ac412d6cd2301b445f8a144c47cb2
Kathy Maloney <ksm.maloney@gmail.com>
2019-09-12 11:03:49 -0600

    Add code for JavaScript Lesson 81-Accessing Object Properties w/Dot Notation, with a note about what I learned

---
+++ 5-Introduction-to-JavaScript/81-Accessing_Object_Properties_with_Dot_Notation.js
@@ -0,0 +1,23 @@
+/*  You access an object's properties in two ways:
+1. dot notation " . "
+2. bracket notation " [ ] "  (similar to arrays)
+
+Dot notation-use when you already know the property name
+
+After you declare your variable, then insert the name of the object followed by a dot (.) then the known property name.
+
+Note that if the property name has quote marks (it's a string), you don't need those quote marks when you write the property name again when you set the variable for accessing the property.
+
+*/
+
+// Setup
+var testObj = {
+  "hat": "ballcap",
+  "shirt": "jersey",
+  "shoes": "cleats"
+};
+
+// Only change code below this line
+
+var hatValue = testObj.hat;      // Change this line
+var shirtValue = testObj.shirt;    // Change this line
