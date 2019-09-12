8a392280ba93d770cb0eeea2789d16b0d4ab9a2d
Kathy Maloney <ksm.maloney@gmail.com>
2019-09-12 16:13:11 -0600

    Add code for JavaScript Lesson 89-Manipulating Complex Objects, plus a comment on what I learned

---
+++ 5-Introduction-to-JavaScript/89-Manipulating_Complex_Objects.js
@@ -0,0 +1,34 @@
+/*  This challenge showed how to create a "complex data structure." This type of data structure can hold strings, numbers, booleans, arrays, functions, and objects.
+
+The test didn't pass until I inserted a comma between the first record and the second.
+
+*/
+
+var myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  }
+
+  // Add record here
+,
+  {
+    "artist": "Stevie Wonder",
+    "title": "Songs in the Key of Life",
+    "release_year": 1976,
+    "formats":  [
+        "CD",
+        "8T",
+        "LP",
+        "Cassette",
+        "Streaming"
+    ],
+  }
+];
