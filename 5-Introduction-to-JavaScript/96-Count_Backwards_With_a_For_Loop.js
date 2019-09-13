29e58e1207cfb3ed9f70e420d9984862778dad62
Kathy Maloney <ksm.maloney@gmail.com>
2019-09-12 21:30:38 -0600

    Add code for JavaScript Lesson 96-Count Backwards w/a For Loop, and add a comment on what I learned

--- 5-Introduction-to-JavaScript/96-Count_Backwards_With_a_For_Loop.js
+++ 5-Introduction-to-JavaScript/96-Count_Backwards_With_a_For_Loop.js
@@ -1,1 +1,17 @@
+//  To count backwards, you need to set the final-expression to decrement with " -= ". You set the initialization to the number where you want to start counting backwards. 
 
+
+// Example
+var ourArray = [];
+
+for (var i = 10; i > 0; i -= 2) {
+  ourArray.push(i);
+}
+
+// Setup
+var myArray = [];
+
+// Only change code below this line.
+for (var i = 9; i > 0; i-=2) {
+    myArray.push(i);
+}
