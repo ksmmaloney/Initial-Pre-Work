7981884e63af7e204bc3316488f03f485162fa7f
Kathy Maloney <ksm.maloney@gmail.com>
2019-09-12 22:25:05 -0600

    Add code for JavaScript Lesson 97-Iterate Through an Array w/ a For Loop, plus a comment on what I learned

---
+++ 5-Introduction-to-JavaScript/97-Iterate_Through_an_Array_with_a_For_Loop.js
@@ -0,0 +1,21 @@
+/*  A for loop can also iterate through the contents of an array. In this case--add the values of each element.
+
+*/
+
+var ourArr = [ 9, 10, 11, 12];
+var ourTotal = 0;
+
+for (var i = 0; i < ourArr.length; i++) {
+  ourTotal += ourArr[i];
+}
+
+// Setup
+var myArr = [ 2, 3, 4, 5, 6];
+
+// Only change code below this line
+var total = 0;
+
+for (var i = 0; i < myArr.length; i++) {
+   total += myArr[i];
+
+}
