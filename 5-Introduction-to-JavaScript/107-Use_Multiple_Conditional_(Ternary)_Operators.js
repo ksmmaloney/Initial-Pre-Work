cb0d44d2b697bb629976207da12b2ae804427360
Kathy Maloney <ksm.maloney@gmail.com>
2019-09-13 20:12:26 -0600

    Add code for JavaScript Lesson 107-Use Multiple Conditional (Ternary) Operators, with a note about what I learned

---
+++ 5-Introduction-to-JavaScript/107-Use_Multiple_Conditional_(Ternary)_Operators.js
@@ -0,0 +1,11 @@
+// You can check for multiple conditions by linking more than one conditional operator together.
+
+
+
+unction checkSign(num) {
+
+  return num > 0 ? "positive" : num < 0 ? "negative" : "zero" ;
+
+}
+
+console.log(checkSign(0));
