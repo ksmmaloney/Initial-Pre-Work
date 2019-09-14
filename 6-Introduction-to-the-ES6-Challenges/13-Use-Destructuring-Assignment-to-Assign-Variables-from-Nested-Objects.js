92cfde53f3bf3938776be9eb58d0b1cb2572be52
Kathy Maloney <ksm.maloney@gmail.com>
2019-09-14 15:20:19 -0600

    Add code for ES6 Lesson 13-Destructuring Assignment to Assign Variables from Nested Objects, plus a note about what I learned

---
+++ 6-Introduction-to-the-ES6-Challenges/13-Use-Destructuring-Assignment-to-Assign-Variables-from-Nested-Objects.js
@@ -0,0 +1,16 @@
+//  You can also destruct nested objects into variables. I finally learned to write out ahead of time what the task is asking for, plus the little segments that will make it work, and piece it together. 
+
+const LOCAL_FORECAST = {
+  today: { min: 72, max: 83 },
+  tomorrow: { min: 73.3, max: 84.6 }
+};
+
+function getMaxOfTmrw(forecast) {
+  "use strict";
+  // change code below this line
+  const { tomorrow : { max : maxOfTomorrow }} = forecast; // change this line
+  // change code above this line
+  return maxOfTomorrow;
+}
+
+console.log(getMaxOfTmrw(LOCAL_FORECAST)); // should be 84.6
