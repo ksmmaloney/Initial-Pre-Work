004b0183152b2b9647ccd763db00943fca9120e4
Kathy Maloney <ksm.maloney@gmail.com>
2019-09-13 23:34:07 -0600

    Add code for ES6 Lesson 7-Write Arrow Functions w/ Parameters, and add a note about what I learned

---
+++ 6-Introduction-to-the-ES6-Challenges/7-Write-Arrow-Functions-with-Parameters.js
@@ -0,0 +1,8 @@
+//  With arrow functions you can still pass one or more arguments into them, just like typical functions.
+
+use strict";
+const myConcat = (arr1, arr2) =>  arr1.concat(arr2);
+
+
+// test your code
+console.log(myConcat([1, 2], [3, 4, 5]));
