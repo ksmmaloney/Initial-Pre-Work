98f4f7bdccec4604428085c38d724d40a73fed78
Kathy Maloney <ksm.maloney@gmail.com>
2019-09-15 00:16:09 -0600

    Add code for ES6 Lesson 15-Use Destructing Assignment with the Rest Operator to Reassign Array Elements, plus a note on what I learned

---
+++ 6-Introduction-to-the-ES6-Challenges/15-Use-Destructuring-Assignment-with-the-Rest-Operator-to-Reassign-Array-Elements.js
@@ -0,0 +1,18 @@
+/*  You can create a separate array that collects "the rest" of an array's elements. This instance allows the input array to have its first two elements taken out and the remaining elements a sub-array.
+
+*/
+
+
+
+
+const source = [1,2,3,4,5,6,7,8,9,10];
+function removeFirstTwo(list) {
+  "use strict";
+  // change code below this line
+  const [a, b, ...arr] = list ; // change this
+  // change code above this line
+  return arr;
+}
+const arr = removeFirstTwo(source);
+console.log(arr); // should be [3,4,5,6,7,8,9,10]
+console.log(source); // should be [1,2,3,4,5,6,7,8,9,10];
