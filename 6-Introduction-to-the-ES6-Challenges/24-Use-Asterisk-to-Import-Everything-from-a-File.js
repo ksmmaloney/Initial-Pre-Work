d09eb910ff8ddf82438d2de812c76b4639419aa4
Kathy Maloney <ksm.maloney@gmail.com>
2019-09-15 21:54:17 -0600

    Add code for ES6 Lesson 24-Use Asterisk to Import Everything from a File, and add a note about what I learned

---
+++ 6-Introduction-to-the-ES6-Challenges/24-Use-Asterisk-to-Import-Everything-from-a-File.js
@@ -0,0 +1,3 @@
+//  You can use the "import *" syntax to import all of a file's contents into the current file. You need to create an object that will receive the imported values. 
+
+import * as myOrangeObject from "capitalize_strings";
