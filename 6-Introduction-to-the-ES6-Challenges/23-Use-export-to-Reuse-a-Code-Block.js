b3893404fd0d9813b162c45a023a254acfcae3cc
Kathy Maloney <ksm.maloney@gmail.com>
2019-09-15 21:32:41 -0600

    Add code for ES6 Lesson 23-Use Export to Reuse a Code Block, plus a note about what I learned

---
+++ 6-Introduction-to-the-ES6-Challenges/23-Use-export-to-Reuse-a-Code-Block.js
@@ -0,0 +1,9 @@
+/*  You can use "export" if you have some code that you want to be usable in another file. It must be exported to another file. Both import and export are non-browser features.
+
+*/
+
+
+"use strict";
+
+export const foo = "bar";
+export const bar = "foo";
