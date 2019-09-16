0e29256389c157edaecd8db4aca7f2c890df1a39
Kathy Maloney <ksm.maloney@gmail.com>
2019-09-15 21:18:45 -0600

    Add code for ES6 Lesson 22-Understand the Difference Between Import and Require, plus a note about what I learned

---
+++ 6-Introduction-to-the-ES6-Challenges/22-Understand-the-Differences-Between-import-and-require.js
@@ -0,0 +1,7 @@
+//  The ES6 "import" tool lets you choose which parts of a file to load into a given file. For readability, remember to keep whitespace surrounding the function name.
+
+"use strict";
+
+import { capitalizeString } from "string_functions"
+
+capitalizeString("hello!");
